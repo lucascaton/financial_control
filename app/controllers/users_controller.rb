774b2e0d9a710f311d8aad85e0e39a794c92db24
Lucas Caton <lucascaton@gmail.com>
2011-11-06 15:49:20 -0200

    Improve user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,45 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params[:user]
+
+    if @user.save
+      flash[:notice] = 'Usuário criado com sucesso.'
+      redirect_to @user
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+
+    if @user.update_attributes params[:user]
+      flash[:notice] = 'Usuário atualizado com sucesso.'
+      redirect_to @user
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find params[:id]
+    # @user.destroy
+    flash[:notice] = 'Usuário desativado com sucesso.'
+    redirect_to users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 FinancialControl::Application.routes.draw do
-  devise_for :users
-  resources :users, :only => [:index]
+  devise_for :users, :controllers => { :registrations => 'users' }
+  resources :users
 
   # root :to => 'welcome#index'
 end
