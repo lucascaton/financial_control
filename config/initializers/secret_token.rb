e119eb4cf2d7a79c7d56eaf2fffbacea48235a21
Lucas Caton <lucascaton@gmail.com>
2011-11-06 11:00:09 -0200

    Initial Rails directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinancialControl::Application.config.secret_token = 'a3111b38b4dee6b34e82ce5e71f7a75544aa6ff70cc6d11029b25e454e4553b74d1d0a76b73457f3cfeae3a4531458409cc740b42f96a790acfb86097dacfafc'
