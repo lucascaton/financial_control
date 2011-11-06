41f2dba60b1175cd0edb839ca2aef966ef2978d3
Lucas Caton <lucascaton@gmail.com>
2011-11-06 18:07:08 -0200

    Include session management feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
     @users = User.order('name')
   end
 
-  def show
-    @user = User.find params[:id]
-  end
-
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FinancialControl::Application.routes.draw do
-  devise_for :users, :controllers => { :registrations => 'users' }
+  devise_for :users, :controllers => { :registrations => 'users', :sessions => 'sessions' }
   resources :users, :except => [:destroy]
 
   root :to => 'pages#index'
