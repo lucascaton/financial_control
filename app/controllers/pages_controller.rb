6a580fbe1f85cae5dd181acc4e688d9fe7551e51
Lucas Caton <lucascaton@gmail.com>
2011-11-06 17:13:23 -0200

    Include pages controller + views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def index; end
+  def licence; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ FinancialControl::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'users' }
   resources :users, :except => [:destroy]
 
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
+
+  match 'licence' => 'pages#licence'
 end
