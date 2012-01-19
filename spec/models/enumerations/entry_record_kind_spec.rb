9da06d927cbe4b62a6b4ab5fba9aa29d3386ed0a
Lucas Caton <lucascaton@gmail.com>
2012-01-19 00:08:35 -0200

    Include EntryRecordKind enumeration

---
+++ spec/models/enumerations/entry_record_kind_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe EntryRecordKind do
+  it_should_be_an_enumeration_for(
+    :unexpected                   => ['unexpected', :unexpected],
+    :created_from_the_fixed_entry => ['created_from_the_fixed_entry', :created_from_the_fixed_entry]
+  )
+end
