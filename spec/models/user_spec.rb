161c4455d7416d376e9d1f0f0ac1ad222dbd1c58
Lucas Caton <lucascaton@gmail.com>
2012-01-27 01:28:08 -0200

    Update code to avoid devise's deprecations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@
 #  id                     :integer         not null, primary key
 #  name                   :string(255)
 #  email                  :string(255)     default(""), not null
-#  encrypted_password     :string(128)     default(""), not null
+#  encrypted_password     :string(255)     default(""), not null
 #  reset_password_token   :string(255)
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
@@ -14,8 +14,8 @@
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string(255)
 #  last_sign_in_ip        :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
+#  created_at             :datetime        not null
+#  updated_at             :datetime        not null
 #  admin                  :boolean         default(FALSE)
 #  active                 :boolean         default(TRUE)
 #
