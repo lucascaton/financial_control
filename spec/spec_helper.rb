026aec477850087799e3d8fca7ac29b935ed7b1c
Lucas Caton <lucascaton@gmail.com>
2012-02-25 23:03:34 -0300

    Adjust in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ require 'rspec/mocks'
 require 'capybara/rspec'
 require 'capybara/rails'
 # require 'fakeweb'
-require 'spec/custom_matchers'
+require 'custom_matchers'
 require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
