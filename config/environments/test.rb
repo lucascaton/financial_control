96de7090513bfbc3a5c2872fa58e6e4ff478d75e
Lucas Caton <lucascaton@gmail.com>
2012-06-20 13:15:08 -0300

    Experiment for possible faster boot time

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,11 @@ FinancialControl::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  # This is an experiment for possible faster boot time (and consequently
+  # faster feedback) when running tests.
+  # https://gist.github.com/2947894
+  def eager_load!; end
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
