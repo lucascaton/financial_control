a8c20a3bc0e39f9ca2f1e9826f127d71c397b479
Lucas Caton <lucascaton@gmail.com>
2012-08-11 18:18:27 -0300

    Replacing poltergeist by selenium for a while :-(

--- spec/integration_tests_helper.rb
+++ spec/integration_tests_helper.rb
@@ -11,11 +11,13 @@ require 'capybara/rails'
 require 'capybara/poltergeist'
 require File.expand_path('../support/factories', __FILE__)
 
+Rails.logger.level = 4 # reduce the IO during tests
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 Dir['./spec/integration/macros/**/*.rb'].each { |f| require f }
 
 Capybara.configure do |config|
-  config.javascript_driver = :poltergeist
+  config.javascript_driver = :selenium # :poltergeist
   config.ignore_hidden_elements = true
   config.default_selector = :css
   config.default_wait_time = 10
@@ -24,6 +26,11 @@ end
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = true
 
   config.include IntegrationHelpers, :type => :request
 
